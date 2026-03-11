2a248f832643dcdda0cfbf6cc73f94b4d4108b61
nathan <nathavieiragg@outlook.com>
2026-03-11 19:06:44 -0300

    V3.0

--- downloader.py
+++ downloader.py
@@ -2,32 +2,68 @@ import yt_dlp
 
 url = input("Cole a URL do vídeo: ")
 
-print("\nEscolha a qualidade:")
-print("1 - 1080p")
-print("2 - 720p")
-print("3 - 480p")
+print("\nO que você deseja baixar?")
+print("1 - Vídeo")
+print("2 - Áudio (MP3)")
 
-escolha = input("Digite o número da qualidade: ")
+tipo = input("Escolha uma opção: ")
 
-qualidades = {
-    "1": "bestvideo[height<=1080]+bestaudio/best[height<=1080]",
-    "2": "bestvideo[height<=720]+bestaudio/best[height<=720]",
-    "3": "bestvideo[height<=480]+bestaudio/best[height<=480]"
-}
+# ==============================
+# DOWNLOAD DE VÍDEO
+# ==============================
 
-formato = qualidades.get(escolha)
+if tipo == "1":
 
-if not formato:
-    print("Qualidade inválida!")
+    print("\nEscolha a qualidade:")
+    print("1 - 1080p")
+    print("2 - 720p")
+    print("3 - 480p")
+
+    escolha = input("Digite o número da qualidade: ")
+
+    qualidades = {
+        "1": "bestvideo[height<=1080]+bestaudio/best[height<=1080]",
+        "2": "bestvideo[height<=720]+bestaudio/best[height<=720]",
+        "3": "bestvideo[height<=480]+bestaudio/best[height<=480]"
+    }
+
+    formato = qualidades.get(escolha)
+
+    if not formato:
+        print("Qualidade inválida!")
+        exit()
+
+    opcoes = {
+        "format": formato,
+        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "ffmpeg_location": "./ffmpeg/bin"
+    }
+
+# ==============================
+# DOWNLOAD DE ÁUDIO
+# ==============================
+
+elif tipo == "2":
+
+    opcoes = {
+        "format": "bestaudio/best",
+        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "ffmpeg_location": "./ffmpeg/bin",
+
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192"
+        }]
+    }
+
+else:
+    print("Opção inválida!")
     exit()
 
-opcoes = {
-    "format": formato,
-    "outtmpl": "downloads/%(title)s.%(ext)s",
-
-    # caminho do ffmpeg dentro do projeto
-    "ffmpeg_location": "./ffmpeg/bin"
-}
+# ==============================
+# EXECUTAR DOWNLOAD
+# ==============================
 
 with yt_dlp.YoutubeDL(opcoes) as ydl:
     info = ydl.extract_info(url, download=False)
